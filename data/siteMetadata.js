cb862dba360c535d469bbd4083bd0c477dbc1601
Jedi Kedy <rahim.mukhtarov@proton.me>
2026-02-02 12:42:22 +0400

    Changes to be committed: 	new file:   data/blog/allergik-xestelikler/allergik-keratit.mdx 	new file:   data/blog/allergik-xestelikler/allergik-kontakt-dermatit.mdx 	new file:   data/blog/allergik-xestelikler/allergik-konyuktivit.mdx 	new file:   data/blog/allergik-xestelikler/allergik-otit.mdx 	new file:   data/blog/allergik-xestelikler/allergik-qastrit.mdx 	new file:   data/blog/allergik-xestelikler/allergik-rinit.mdx 	new file:   data/blog/allergik-xestelikler/allergik-rinosinusopatiya.mdx 	deleted:    data/blog/code-sample.mdx 	deleted:    data/blog/deriving-ols-estimator.mdx 	deleted:    data/blog/github-markdown-guide.mdx 	deleted:    data/blog/guide-to-using-images-in-nextjs.mdx 	deleted:    data/blog/introducing-tailwind-nextjs-starter-blog.mdx 	deleted:    data/blog/my-fancy-title.mdx 	deleted:    data/blog/nested-route/introducing-multi-part-posts-with-nested-routing.mdx 	deleted:    data/blog/new-features-in-v1.mdx 	deleted:    data/blog/pictures-of-canada.mdx 	deleted:    data/blog/release-of-tailwind-nextjs-starter-blog-v2.0.mdx 	deleted:    data/blog/the-time-machine.mdx 	modified:   data/siteMetadata.js

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,13 +1,13 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
-  description: 'A blog created with Next.js and Tailwind.css',
-  language: 'en-us',
+  title: 'MedBlog',
+  author: 'JediKedy',
+  headerTitle: 'Medblog',
+  description: 'Tibbi mövzularda bloq',
+  language: 'az-AZ',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
-  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
+  siteUrl: 'https://jedikedy.github.io/medblog/',
+  siteRepo: 'https://github.com/JediKedy/medblog',
   siteLogo: `${process.env.BASE_PATH || ''}/static/images/logo.png`,
   socialBanner: `${process.env.BASE_PATH || ''}/static/images/twitter-card.png`,
   mastodon: 'https://mastodon.social/@mastodonuser',
